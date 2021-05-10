e1772d21da50f633eba993ac996f0ab97df57ada
Qutub <qutubhaider950@gmail.com>
2021-05-10 22:21:53 +0530

    Driver Action for Ionic App

---
+++ APIKwickWash/Controllers/DriverActionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using APIKwickWash.Models;
+namespace APIKwickWash.Controllers
+{
+    
+    public class DriverActionController : Controller
+    {
+        public string Post([FromBody] CreateAccept Data)
+        {
+            string res = string.Empty;
+            try
+            {
+                string query = "INSERT INTO TBL.KWICKORDER isAccept='" + Data.isAccept + "' where KOID='" + Data.orderId + "'";
+                int result = Database.Execute(query);
+                if (result == 1)
+                {
+                    res = "1";
+                }
+                else
+                {
+                    res = "0";
+                }
+            }
+            catch(Exception ex)
+            {
+                res = "00";
+            }
+            return res;
+        }
+    }
+}
